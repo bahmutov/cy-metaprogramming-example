ef56678bfb39b287baf19516e9bdc69ddaa32a05
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-11 21:40:40 -0500

    spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,3 +5,27 @@
 it('works', () => {
   expect(true).to.be.true
 })
+
+afterEach(function () {
+  console.log(this.currentTest)
+})
+
+const emergency = () => {
+  throw new Error('Should never happen')
+}
+
+
+it('fails with expected 5 to equal 42', () => {
+  cy.on('fail', (e, test) => {
+    if (test.title.startsWith('fails with') &&
+      test.title.endsWith(e.message)) {
+      console.log('test "%s" is allowed to fail with error "%s"',
+        test.title, e.message)
+    } else {
+      throw e
+    }
+  })
+
+  // expect(5).to.equal(42)
+  emergency()
+})
