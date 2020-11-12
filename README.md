fab70ff683aa2c3f30780c2271aab120606290c3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-11 22:22:15 -0500

    add cypress-expect

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cy-metaprogramming-example
 
 > Example testing the tests
+
+See [cypress/integration/spec.js](cypress/integration/spec.js)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,30 +2,45 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+//
+// enable if you want to pass tests that fail with expected error
+//
+// const failedTests = {}
+// const isFailingTest = (test) =>
+//   test.title.startsWith('fails with')
+
+// afterEach(function () {
+//   console.log(this.currentTest)
+//   console.log(failedTests)
+//   if (!isFailingTest(this.currentTest)) {
+//     return // all good
+//   }
+//   if (!failedTests[this.currentTest.title]) {
+//     throw new Error(`Test "${this.currentTest.title}" somehow passed`)
+//   }
+// })
+
+// Cypress.on('fail', (e, test) => {
+//   if (isFailingTest(test) &&
+//     test.title.endsWith(e.message)) {
+//     console.log('test "%s" is allowed to fail with error "%s"',
+//       test.title, e.message)
+//     failedTests[test.title] = test
+//   } else {
+//     throw e
+//   }
+// })
+
 it('works', () => {
   expect(true).to.be.true
 })
 
-afterEach(function () {
-  console.log(this.currentTest)
+it('fails with expected 5 to equal 42', () => {
+  expect(5).to.equal(42)
 })
 
-const emergency = () => {
-  throw new Error('Should never happen')
-}
-
-
-it('fails with expected 5 to equal 42', () => {
-  cy.on('fail', (e, test) => {
-    if (test.title.startsWith('fails with') &&
-      test.title.endsWith(e.message)) {
-      console.log('test "%s" is allowed to fail with error "%s"',
-        test.title, e.message)
-    } else {
-      throw e
-    }
-  })
-
-  // expect(5).to.equal(42)
-  emergency()
+it.skip('fails with some error', () => {
+  // notice that this test should have failed,
+  // judging by the title, but does not
+  expect(5).to.equal(5)
 })
